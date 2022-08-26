f36b03c0dc2f987df3ee6a68a47626838131df08
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-08-26 09:15:03 +0200

    Initial commit

---
+++ src/main/kotlin/com/jorgeparavicini/springwebshop/SpringWebShopApplication.kt
@@ -0,0 +1,11 @@
+package com.jorgeparavicini.springwebshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebShopApplication>(*args)
+}
