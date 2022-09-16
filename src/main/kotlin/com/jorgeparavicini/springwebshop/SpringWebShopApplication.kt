eaeba621c5c2ef30136c5bcd71d0c8b6a11f99dd
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-09-16 12:41:13 +0200

    Cleanup Main

--- src/main/kotlin/com/jorgeparavicini/springwebshop/SpringWebShopApplication.kt
+++ src/main/kotlin/com/jorgeparavicini/springwebshop/SpringWebShopApplication.kt
@@ -1,7 +1,7 @@
 package com.jorgeparavicini.springwebshop
 
-import com.jorgeparavicini.springwebshop.database.entities.Customer
-import com.jorgeparavicini.springwebshop.database.repositories.CustomerRepository
+import com.jorgeparavicini.springwebshop.database.entities.ProductCategory
+import com.jorgeparavicini.springwebshop.database.repositories.ProductCategoryRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
@@ -14,40 +14,13 @@ class SpringWebShopApplication {
     private val log: Logger = LoggerFactory.getLogger(SpringWebShopApplication::class.java)
 
     @Bean
-    fun demo(repository: CustomerRepository): CommandLineRunner {
-        return CommandLineRunner { _ ->
+    fun seedProductCategory(repo: ProductCategoryRepository): CommandLineRunner {
+        return CommandLineRunner {_ ->
+            repo.save(ProductCategory("Test", "Test"))
 
-            // save a few customers
-            repository.save(Customer("Jack", "Bauer"))
-            repository.save(Customer("Chloe", "O'Brian"))
-            repository.save(Customer("Kim", "Bauer"))
-            repository.save(Customer("David", "Palmer"))
-            repository.save(Customer("Michelle", "Dessler"))
-
-            // fetch all customers
-            log.info("Customers found with findAll():")
-            log.info("-------------------------------")
-            for (customer in repository.findAll()) {
-                log.info(customer.toString())
+            for (category in repo.findAll()) {
+                log.info("Category: ${category.id}")
             }
-            log.info("")
-
-            // fetch an individual customer by ID
-            val customer = repository.findById(1L)
-            log.info("Customer found with findById(1L):")
-            log.info("--------------------------------")
-            log.info(customer.toString())
-            log.info("")
-
-            // fetch customers by last name
-            log.info("Customer found with findByLastName('Bauer'):")
-            log.info("--------------------------------------------")
-            repository.findByLastName("Bauer").forEach { bauer -> log.info(bauer.toString()) }
-            // for (Customer bauer : repository.findByLastName("Bauer")) {
-            //  log.info(bauer.toString());
-            // }
-            log.info("")
-
         }
     }
 }
