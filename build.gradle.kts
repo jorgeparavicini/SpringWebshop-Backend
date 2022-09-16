cf45bb0ed55385f52a4fe131331cb5fad09e645e
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-09-16 12:00:33 +0200

    Remove unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     // Other dependencies
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
