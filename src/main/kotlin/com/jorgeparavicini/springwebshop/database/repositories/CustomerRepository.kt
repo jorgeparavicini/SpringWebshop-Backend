89b50cf52f6f66842ec10b226ef5a7efe6cd9766
Jorge Paravicini <j.m.paravicini@gmail.com>
2022-09-16 12:39:45 +0200

    Remove unused features

--- src/main/kotlin/com/jorgeparavicini/springwebshop/database/repositories/CustomerRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.jorgeparavicini.springwebshop.database.repositories
-
-import com.jorgeparavicini.springwebshop.database.entities.Customer
-import org.springframework.data.repository.CrudRepository
-
-interface CustomerRepository: CrudRepository<Customer, Long> {
-    fun findByLastName(lastName: String): List<Customer>
-}
